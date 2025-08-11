718a56bece18bc903c90d45799d59bf53148ca80
ValentinoOnofri <vonofri@mail.austral.edu.ar>
2025-08-11 12:24:46 -0300

    Add files via upload

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,11 @@
 def math():
     a = 57
     b = 7
+    print(a+b) #Suma
+    print(a-b) #Resta
+    print(a*b) #Producto
+    print((a+b)/2) #Promedio
+    print(a//b) #Division entera
+    print(a%b) #Resto
+    print(a/b) #Valor real de la division
+#math()
